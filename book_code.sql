eeb0edf717d591a99d44fd775f2fea4fb7aaba98
root <root@localhost.localdomain>
2018-06-28 00:23:09 +0100

    Adding new mysql code

---
+++ book_code.sql
@@ -0,0 +1,12 @@
+-- SELECT 
+--     CONCAT
+--     (
+--         SUBSTRING(title,1,10),
+--         '...'
+--     ) AS 'short title'
+-- FROM books;
+
+SELECT
+    SUBSTRING(REPLACE(title, 'e', '3'),1,10) AS 'wierd Strings'
+    FROM books;
+    
